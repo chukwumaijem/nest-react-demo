892d085a092879a9682b259c9d8c2d43c36d589c
Chukwuma Ezumezu <chukwuma.ezumezu@gmail.com>
2020-06-06 12:32:36 +0100

    test signup and login end to end

---
+++ server/test/db.cleanup.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { Connection } from 'mongoose';
+import { InjectConnection } from '@nestjs/mongoose';
+
+import { Auth } from '../src/modules/auth/schema/auth.schema';
+
+@Injectable()
+export class DatabaseCleanUpService {
+  constructor(@InjectConnection() private connection: Connection) {}
+
+  async removeCollection() {
+    await this.connection.models[Auth.name].remove({});
+  }
+
+  async dropDatabase() {
+    this.connection.dropDatabase();
+  }
+}
